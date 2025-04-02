8be121dde79f15da66b092ea5e226e00443c7500
fbartelt <fbarteltp@gmail.com>
2025-04-02 13:43:58 -0300

    Feat: improve animation + animate distance/error results

--- check_experimental_results.py
+++ check_experimental_results.py
@@ -41,8 +41,14 @@ for i, q in enumerate(config_hist[:-1]):
 # makes a figure with two plots, one above another. First the position error, then the orientation error
 dt = 0.01
 # time_vec = np.arange(0, len(pos_errs) * dt, dt)
-time_vec = np.arange(0, len(pos_errs))
-time_vec = hist_time
+# time_vec = np.arange(0, len(pos_errs))
+
+final_index = np.nonzero(np.array(hist_time) > 150)[0][0]
+init_index = np.nonzero(np.array(hist_dist) > 0.3961)[0][0]
+time_vec = np.array(hist_time[init_index:final_index]) - hist_time[init_index]
+hist_dist = hist_dist[init_index:final_index]
+pos_errs = pos_errs[init_index:final_index]
+ori_errs = ori_errs[init_index:final_index]
 fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.02)
 fig.add_trace(go.Scatter(x=time_vec, y=hist_dist, showlegend=False, line=dict(width=3)), row=1, col=1)
 fig.add_trace(go.Scatter(x=time_vec, y=pos_errs, showlegend=False, line=dict(width=3)), row=2, col=1)
@@ -55,7 +61,139 @@ fig.update_yaxes(title_text="Pos. error (cm)", gridcolor='gray', zerolinecolor='
 fig.update_yaxes(title_text="Ori. error (deg)", gridcolor='gray', zerolinecolor='gray', row=3, col=1, title_standoff=30)
 fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
 
+# fig.update_layout(plot_bgcolor='white', paper_bgcolor='white',
+#                   width=718.110, height=605.9155)
 fig.update_layout(plot_bgcolor='white', paper_bgcolor='white',
-                  width=718.110, height=605.9155)
-fig.show()
+                  width=800, height=600)
+# fig.show()
+# %%
+"""CREATE ANIMATION"""
+def animate_distance(distances, pos_errors, ori_errors, time_data, fig=None):
+    """Create an animation of the distance metric between the object and the
+    target curve, along with the position and orientation errors.
+
+    Parameters
+    ----------
+    distances : list or np.ndarray
+        List of EC-distances between the object and the target curve.
+    pos_errors : list or np.ndarray
+        List of position errors in centimeters.
+    ori_errors : list or np.ndarray
+        List of orientation errors in degrees.
+    time_data : list or np.ndarray
+        List of time values.
+    fig : plotly.graph_objects.Figure, optional
+        Existing figure to add the animation to. If None, a new figure is
+        created. The default is None.
+
+    Returns
+    -------
+    fig : plotly.graph_objects.Figure
+        Resulting plotly figure.
+    """
+    width_ = 2
+    gridcolor = 'rgba(0, 0, 0, 0.2)'
+    if fig is None:
+        fig = make_subplots(rows=3, cols=1, shared_xaxes=True, vertical_spacing=0.02)
+        fig.add_trace(go.Scatter(x=time_data, y=distances, showlegend=False, line=dict(width=3)), row=1, col=1)
+        fig.add_trace(go.Scatter(x=time_data, y=pos_errors, showlegend=False, line=dict(width=3)), row=2, col=1)
+        fig.add_trace(go.Scatter(x=time_data, y=ori_errors, showlegend=False, line=dict(width=3)), row=3, col=1)
+        fig.update_xaxes(title_text="Time (s)", gridcolor=gridcolor, zerolinecolor='gray', zerolinewidth=width_, gridwidth=width_, row=3, col=1)
+        fig.update_xaxes(title_text="", gridcolor=gridcolor, zerolinecolor='gray', zerolinewidth=width_, gridwidth=width_, row=1, col=1)
+        fig.update_xaxes(title_text="", gridcolor=gridcolor, zerolinecolor='gray', zerolinewidth=width_, gridwidth=width_, row=2, col=1)
+        fig.update_yaxes(title_text="Distance D", gridcolor=gridcolor, zerolinecolor='gray', zerolinewidth=width_, gridwidth=width_, row=1, col=1, title_standoff=30)
+        fig.update_yaxes(title_text="Pos. error (cm)", gridcolor=gridcolor, zerolinecolor='gray', zerolinewidth=width_, gridwidth=width_, row=2, col=1, title_standoff=30)
+        fig.update_yaxes(title_text="Ori. error (deg)", gridcolor=gridcolor, zerolinecolor='gray', zerolinewidth=width_, gridwidth=width_, row=3, col=1, title_standoff=30)
+        fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), font=dict(size=16))
+        fig.update_layout(plot_bgcolor='white', paper_bgcolor='white', width=1480, height=960)
+    
+    print("Creating frames")
+    frames = [go.Frame(data=[
+            go.Scatter(x=time_data[:k], y=distances[:k], showlegend=False, line=dict(width=3), mode='lines'),
+            go.Scatter(x=time_data[:k], y=pos_errors[:k], showlegend=False, line=dict(width=3), mode='lines'),
+            go.Scatter(x=time_data[:k], y=ori_errors[:k], showlegend=False, line=dict(width=3), mode='lines'),
+        ], name=f'frame_{k}') for k in range(len(time_data))]
+    
+    fig.update(frames=frames)
+    print("Addind layout")
+    layout=go.Layout(
+            # width=600,
+            # height=600,
+            # margin=dict(r=5, l=5, b=5, t=5),
+            # xaxis=dict(range=[0, time_data[-1]], autorange=False, title="Time (s)"),
+            # yaxis=dict(
+            #     range=[0, np.], autorange=False, title="Value of metric <i>D</i>"
+            # ),
+            xaxis=dict(range=[-0.1, time_data[-1]], autorange=False),
+            # xaxis=dict(autorange=True),
+            # xaxis2=dict(zeroline=False),
+            # xaxis3=dict(zeroline=False),
+            yaxis=dict(range=[-1.1*np.min(distances), np.max(distances)*1.1], autorange=False),
+            yaxis2=dict(range=[-1.1*np.min(pos_errors), np.max(pos_errors)*1.1], autorange=False),
+            yaxis3=dict(range=[-1.1*np.min(ori_errors), np.max(ori_errors)*1.1], autorange=False),
+            updatemenus=[
+                dict(
+                    type="buttons",
+                    buttons=[
+                        dict(
+                            label="Play",
+                            method="animate",
+                            args=[
+                                None,
+                                {
+                                    "frame": {"duration": 82.9999, "redraw": False},
+                                    "fromcurrent": True,
+                                    "transition": {
+                                        "duration": 0.01,
+                                        "easing": "cubic-in-out",
+                                    },
+                                },
+                            ],
+                        ),
+                        dict(
+                            label="Pause",
+                            method="animate",
+                            args=[
+                                [None],
+                                {
+                                    "frame": {"duration": 0, "redraw": False},
+                                    "mode": "immediate",
+                                    "transition": {"duration": 0},
+                                },
+                            ],
+                        ),
+                    ],
+                    direction= "left",
+                    pad= {"r": 10, "t": 87},
+                    showactive= False,
+                    x= 0.1,
+                    xanchor= "right",
+                    y= 0,
+                    yanchor= "top"
+                )
+            ]
+    )
+
+    fig.update_layout(layout)
+    # fig.update_xaxes(zeroline=True, row=1, col=1, zerolinecolor='gray', gridwidth=1.2, gridcolor='gray')
+    # fig.update_xaxes(zeroline=True, row=2, col=1, zerolinecolor='gray', gridwidth=1.2, gridcolor='gray')
+    # fig.update_xaxes(zeroline=True, row=3, col=1, zerolinecolor='gray', gridwidth=1.2, gridcolor='gray')
+    # fig.update_yaxes(zeroline=True, row=1, col=1, zerolinecolor='gray', gridwidth=1.2, gridcolor='gray')
+    # fig.update_yaxes(zeroline=True, row=2, col=1, zerolinecolor='gray', gridwidth=1.2, gridcolor='gray')
+    # fig.update_yaxes(zeroline=True, row=3, col=1, zerolinecolor='gray', gridwidth=1.2, gridcolor='gray')
+
+    return fig
+
+skip = 10
+time_data = time_vec[::skip]
+pos_errors = pos_errs[::skip]
+ori_errors = ori_errs[::skip]
+distances = hist_dist[::skip]
+
+fig = animate_distance(distances, pos_errors, ori_errors, time_data, fig=None)
+# fig.show()
+# %%
+import plotly.io as pio
+# fig.update_layout(transition = {'duration': 90})
+pio.write_html(fig, file='./plotly_animation.html', auto_play=False)
 # %%
--- experiment_animation.py
+++ experiment_animation.py
@@ -2,12 +2,14 @@
 import pickle
 import numpy as np
 from uaibot import Robot, Utils, Simulation, PointCloud, Frame
+from scipy.interpolate import interp1d
 
 def config_mapping(q, maptype="from_kinova"):
     if maptype == "from_kinova":
         q = np.deg2rad(np.array(q).ravel())
         delta = np.array([0] + [np.pi] * 6).ravel()
-        q = q + delta
+        q = np.unwrap(q + delta)
+        q = (q + np.pi) % (2 * np.pi) - np.pi 
     else:
         # maptype == 'to_kinova'
         q = np.rad2deg(np.array(q).ravel())
@@ -26,12 +28,12 @@ def get_points_from_curve(curve):
 # with open('mapped_data.pkl', 'wb') as f:
 #     data = {'config_hist':q_, 'time_hist': data['time_hist']}
 #     pickle.dump(data, f)
-# f_type = 'config' # config
-# with open(f'{f_type}_data.pkl', 'rb') as f:
-#     data = pickle.load(f)
+file_name = 'config' # config
+with open(f'data.pkl', 'rb') as f:
+    data = pickle.load(f)
 
-# config_hist = data['config_hist']
-# time_hist = data['time_hist']
+config_hist = data['config_hist']
+time_hist = data['hist_time']
 
 config_hist = np.load('config_hist_exp.npy')
 time_hist = np.load('time_hist_exp.npy')
@@ -44,8 +46,8 @@ print("kinova created")
 point_mat = get_points_from_curve(curve)
 target = PointCloud(name="target", points=point_mat, size=0.01, color="cyan")
 
-kinova.set_ani_frame(q=config_mapping([0, 0, 0, 5, 0, 10, 0], "from_kinova"))
-kinova.set_ani_frame(q=config_mapping([0, 10, 0, 15, 0, 40., 180.72], "from_kinova"))
+# kinova.set_ani_frame(q=config_mapping([0, 0, 0, 5, 0, 10, 0], "from_kinova"))
+kinova.set_ani_frame(q=config_mapping([0, 10, 0, 15, 0, 40, 30], "from_kinova"))
 
 sim = Simulation.create_sim_grid([kinova, target])
 
@@ -63,14 +65,58 @@ print("frames")
 for i, htm in enumerate(frame_htms):
     frame = Frame(htm, name=f"frame_{i}", size=0.1)
     frames.append(frame)
-
+del frames[1]
 sim.add(frames)
-
-for i, q_ in enumerate(config_hist):
-    q_rad = config_mapping(q_, "from_kinova")
-    kinova.add_ani_frame(time=time_hist[i], q=q_rad)
-    target.add_ani_frame(time=time_hist[i], initial_ind=0, final_ind=len(curve) - 1)
-
+# sim.set_parameters(ambient_light_intensity=0.01)
 sim.run()
+#%%
+# # Interpolate configurations for smoother movement:
+time_hist = np.array(time_hist)
+# config_hist = np.array(config_hist)
+config_hist = np.array([config_mapping(q, "from_kinova") for q in config_hist])
+fps = 200
+config_upsampled = []
+t_upsampled = []
+
+def angle_interp(start, end, t):
+    qs = []
+    start = np.unwrap(start)
+    end = np.unwrap(end)
+    # at = np.arange(0, 1, ratio)
+    for t_ in t:
+        q_ = start + ((((((end - start) % (2 * np.pi)) + (3 * np.pi)) % (2 * np.pi)) - np.pi) / (t[-1] - t[0])) * (t_ - t[0])
+        qs.append(q_)
+    return qs
+
+for i, t in enumerate(time_hist[:-1]):
+    t_eval = np.arange(t, time_hist[i + 1], 1/fps)
+    # q_interval = np.array([config_hist[i, :], config_hist[i + 1, :]]).T
+    # f_interp = interp1d([t, time_hist[i + 1]], q_interval, kind='linear', fill_value='extrapolate')
+    # q_ = (np.unwrap(f_interp(t_eval).T) + np.pi) % (2 * np.pi) - np.pi
+    q_ = angle_interp(config_hist[i, :], config_hist[i + 1, :], t_eval)
+    config_upsampled.extend(q_)
+    t_upsampled.extend(t_eval)
+
+t_upsampled.append(time_hist[-1])
+config_upsampled.append(config_hist[-1])
+
+config_upsampled = np.array(config_upsampled)
+t_upsampled = np.array(t_upsampled)
+
+# t_upsampled = np.arange(0, time_hist[-1], 1/fps)
+# f_interp = interp1d(time_hist, config_hist.T, kind='cubic', fill_value='extrapolate')
+# config_upsampled = f_interp(t_upsampled).T
+
+# final_index = np.nonzero(np.array(time_hist) > 180)[0][0]
+final_index = np.nonzero(np.array(t_upsampled) > 180)[0][0]
+# for i, q_ in enumerate(config_hist[:final_index]):
+for i, q_ in enumerate(config_upsampled[:final_index]):
+    # q_rad = config_mapping(q_, "from_kinova")
+    # kinova.add_ani_frame(time=time_hist[i], q=q_)
+    # target.add_ani_frame(time=time_hist[i], initial_ind=0, final_ind=len(curve) - 1)
+    kinova.add_ani_frame(time=t_upsampled[i], q=q_)
+    target.add_ani_frame(time=t_upsampled[i], initial_ind=0, final_ind=len(curve) - 1)
+sim.set_parameters(width=1480, height=960)
+sim.save('./', 'real_exp')
 
 # %%
